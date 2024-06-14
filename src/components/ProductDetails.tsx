ddf0e728a60839fd522ba4acab98aac5d56360b5
George Kolesnikov <gkolesnikov88@gmail.com>
2024-06-14 06:31:15 +0200

    Add images block

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import "./_productDetails.css";
 import { ProductDetailsType } from "./ProductTypes";
 
 type ProductDetailsProps = {
-    productData: ProductDetailsType;
-}
+  productData: ProductDetailsType;
+};
 
-export const ProductDetails = ({
-  productData
-}: ProductDetailsProps) => {
+export const ProductDetails = ({ productData }: ProductDetailsProps) => {
   console.log(productData);
+  const [currentImage, setCurrentImage] = useState(
+    productData.images?.length ? 0 : -1
+  );
 
-  return <>
-    <section >
-        ProductDetails
-    </section>
-  </>;
+  return (
+    <>
+      <section className="product-details">
+        <div className="images__wrapper">
+          <div className="current-image__wrapper">
+            <img
+              className="current-image"
+              src={`${
+                currentImage >= 0
+                  ? productData.images?.[currentImage].image_url
+                  : ""
+              }`}
+              alt="Main image"
+            />
+          </div>
+          <div className="thumbnails__wrapper">
+            {productData.images?.filter((_, idx) => idx > 0).map((thumbnail, idx) => (
+              <div className="thumbnails" key={idx}>
+                <img
+                  className={`thumbnail__image ${idx === 0 ? "thumbnail__image--active": ""}`}
+                  src={thumbnail.image_url}
+                  alt={`Thumbnail image # ${idx + 1}`}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
