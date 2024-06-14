98ee621977708e8abc0afe1dc34b4213ad78eb65
George Kolesnikov <gkolesnikov88@gmail.com>
2024-06-14 08:28:29 +0200

    Refactor Images component

---
+++ src/components/Images.tsx
@@ -0,0 +1,40 @@
+import { ProductDetailsType } from "./ProductTypes";
+import "./_images.css"
+
+type ImagesPropsTypes = {
+  productData: ProductDetailsType;
+  currentImage: number;
+};
+
+export const Images = ({ productData, currentImage }: ImagesPropsTypes) => {
+  return (
+    <div className="images__wrapper">
+      <div className="current-image__wrapper">
+        <img
+          className="current-image"
+          src={`${
+            currentImage >= 0
+              ? productData.images?.[currentImage].image_url
+              : ""
+          }`}
+          alt="Main image"
+        />
+      </div>
+      <div className="thumbnails__wrapper">
+        {productData.images
+          ?.filter((_, idx) => idx > 0)
+          .map((thumbnail, idx) => (
+            <div className="thumbnails" key={idx}>
+              <img
+                className={`thumbnail__image ${
+                  idx === 0 ? "thumbnail__image--active" : ""
+                }`}
+                src={thumbnail.image_url}
+                alt={`Thumbnail image # ${idx + 1}`}
+              />
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -3,51 +3,14 @@ import "./_productDetails.css";
 import "./_productContent.css";
 import { ProductDetailsType } from "./ProductTypes";
 import { PriceInfo } from "./PriceInfo";
+import { Images } from "./Images";
 
 type ProductDetailsProps = {
   productData: ProductDetailsType;
 };
 
-type ImagesPropsTypes = {
-  productData: ProductDetailsType;
-  currentImage: number;
-};
-
 type ProductContentPropTypes = {
   productData: ProductDetailsType;
-};
-
-const Images = ({ productData, currentImage }: ImagesPropsTypes) => {
-  return (
-    <div className="images__wrapper">
-      <div className="current-image__wrapper">
-        <img
-          className="current-image"
-          src={`${
-            currentImage >= 0
-              ? productData.images?.[currentImage].image_url
-              : ""
-          }`}
-          alt="Main image"
-        />
-      </div>
-      <div className="thumbnails__wrapper">
-        {productData.images
-          ?.filter((_, idx) => idx > 0)
-          .map((thumbnail, idx) => (
-            <div className="thumbnails" key={idx}>
-              <img
-                className={`thumbnail__image ${
-                  idx === 0 ? "thumbnail__image--active" : ""
-                }`}
-                src={thumbnail.image_url}
-                alt={`Thumbnail image # ${idx + 1}`}
-              />
-            </div>
-          ))}
-      </div>
-    </div>
-  );
 };
 
 const ProductContent = ({ productData }: ProductContentPropTypes) => {
