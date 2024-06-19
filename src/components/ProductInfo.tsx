fb6dfb4c6882976e3f92ff6dc1e5c0d3a0ea94d7
George Kolesnikov <gkolesnikov88@gmail.com>
2024-06-19 07:39:31 +0200

    Add collapse for the Info lists

--- src/components/ProductInfo.tsx
+++ src/components/ProductInfo.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./_productInfo.css";
 import { InfoEntity } from "./ProductTypes";
-// import { RiAddCircleLine } from "@remixicon/react";
+import { RiAddCircleLine } from "@remixicon/react";
 import { RiIndeterminateCircleLine } from "@remixicon/react";
 
 type ProductInfoPropTypes = {
@@ -12,17 +12,38 @@ type ProductInfoItemTypes = {
 };
 
 const ProductInfoItem = ({ info }: ProductInfoItemTypes) => {
+  const [collapsed, setCollapsed] = useState(false);
+  const [initialHeight, setInitialHeight] = useState<number | undefined>(undefined);
+  const listRef = useRef(null);
+
+  useEffect(() => {
+    if(listRef?.current) {
+      setInitialHeight((listRef?.current as HTMLUListElement).offsetHeight);
+    }
+  }, []);
+
   return (
     <div className="product-info__container">
       <div className="product-info__header text-lg">
         {info.title}
-        <button className="product-info__header__toggle">
-          <RiIndeterminateCircleLine />
-        </button>        
+        <button
+          className="product-info__header__toggle"
+          onClick={() => {
+            setCollapsed(!collapsed);
+          }}
+        >
+          {collapsed ? <RiAddCircleLine /> : <RiIndeterminateCircleLine />}
+        </button>
       </div>
-      <ul className="product-info__list">
+      <ul ref={listRef} className={`product-info__list ${collapsed ? "collapsed" : ""}`} style={{
+        height: initialHeight + 'px'
+      }}>
         {info.description?.map((description, idx) => {
-          return <li key={idx} className="product-info__list__item text-base">{description}</li>;
+          return (
+            <li key={idx} className="product-info__list__item text-base">
+              {description}
+            </li>
+          );
         })}
       </ul>
     </div>
